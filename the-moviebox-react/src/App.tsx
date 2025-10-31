febe90217c4d1a7abd228ad093c591891fbb6212
heschaefer <168488654+heschaefer@users.noreply.github.com>
2025-10-30 21:57:15 -0300

    Aula 30-10-25

--- the-moviebox-react/src/App.tsx
+++ the-moviebox-react/src/App.tsx
@@ -1,17 +1,17 @@
-<<<<<<< HEAD
-import Footer from "./components/layout/footer"
+import { MovieContent } from "./cases/movies/components/movie-content"
+import  Footer  from "./components/layout/footer"
 import { Header } from "./components/layout/header"
-=======
-import Footer from "./components/layout/Footer"
-import { Header } from "./components/layout/Header"
->>>>>>> 7286178939b146fa62a7a48f64e8611b85f17ecb
+import { Navbar } from "./components/layout/navbar"
 
 function App() {
 
   return (
     <div>
       <Header />
-      <main></main>
+      <main>
+        <Navbar />
+        <MovieContent />
+      </main>
       <Footer />
     </div>
   )
---
+++ the-moviebox-react/src/components/layout/navbar.tsx
@@ -0,0 +1,36 @@
+const categories = [
+    {label: 'Mais Assistidos', active: true, link: '/mais-assistidos'},
+    {label: 'Mas Votados', active: false, link: '/mas-votados'},
+    {label: 'Novidades', active: false, link: '/novidades'},
+    {label: 'Top 10', active: false, link: '/top-10'},
+    {label: 'Gêneros', active: false, link: '/generos'},
+];
+
+export function Navbar() {
+    return (
+     <nav className="flex bg-[#1c1c1c] border-b border-b-[#333] max-w-6xl mx-auto">
+        <ol className="flex gap-8 list-none py-6">
+            {categories.map((item)=>(
+                <li>
+                    <a  
+                        href={item.link} 
+                        className={`text-sm
+                        ${item.active ? 'text-[#e50914] border-b-2 border-[#e50914] hover:text-[#e50914]' :
+                                'text-[#ccc] hover:text-white'}`}>{item.label}
+                        </a>
+                </li>
+            ))}
+            <li>
+            <a href="#">Mais Votados</a>
+            </li>
+            <li>
+            <a href="#">Novidades</a>
+            </li>
+            <li>
+            <a href="#">Gêneros</a>
+            </li>
+        </ol>
+    </nav>
+
+    )
+}
