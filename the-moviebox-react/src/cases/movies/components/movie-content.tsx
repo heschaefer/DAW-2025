012a183f9854e0db3b7ffc88b05307677586d376
heschaefer <168488654+heschaefer@users.noreply.github.com>
2025-11-06 21:57:08 -0300

    Aula 06-11-2025

--- the-moviebox-react/src/cases/movies/components/movie-content.tsx
+++ the-moviebox-react/src/cases/movies/components/movie-content.tsx
@@ -1,27 +1,37 @@
-import { useEffect, useState } from "react"
+import { use, useEffect, useState } from "react"
 import { MovieService, type MovieDTO } from "../services/movie.service"
+import { MovieCard } from "./movie-card";
 
 export function MovieContent() {
 
     const [movies, setMovies] = useState<MovieDTO[]>([])
+    const [isLoading, setisLoading] = useState<boolean>(false);
 
     useEffect(()=>{
+        setisLoading(true);
         MovieService.list()
             .then((result) => {
                 setMovies(result)
             })
+            .finally(() => {
+                setisLoading(false);
+            });
             
     }, []);
 
     return (
+        isLoading ? (
+        <div className="bg-[#1c1c1c] flex justify-center items-center h-40"><p>Loading...</p></div>
+        ) :
         <section
-            className="bg-[#1c1c1c] p-8 grid grid-cols-[repeat(auto-fill, minmax(200px, 1fr))] gap-8 max-w-6xl mx-auto"
+            className="bg-[#1c1c1c] py-12 grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-8 max-w-6xl mx-auto"
 
 
         >
 
-            <p>{JSON.stringify(movies)}</p>
-
+            {movies.map((movie, index) => (
+                <MovieCard key={index} movie={movie} />
+            ))}
         </section>
     )
 }
